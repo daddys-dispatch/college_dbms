64d21ed9c54d6ad2553be38cac9c7f425b1e10e1
Gaurav Chauhan <107812523+grvchnx@users.noreply.github.com>
2025-05-22 20:33:06 +0530

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -1,37 +1,41 @@
--- Create a table Employee with attributes: Id, Name, Salary
+DROP TABLE Employee;
+
+-- Create a table with required attributes
 CREATE TABLE Employee (
     Id NUMBER PRIMARY KEY,
     Name VARCHAR2(50),
     Salary NUMBER
 );
+SELECT * FROM Employee;
 
--- Create TRIGGER to show Old Salary, New Salary, Salary Difference
+-- Create TRIGGER to show required information
 CREATE OR REPLACE TRIGGER Salary_Difference_Trigger
 AFTER INSERT OR UPDATE OR DELETE ON Employee
 FOR EACH ROW DECLARE
     Old_Salary Employee.Salary%TYPE := 0;
     New_Salary Employee.Salary%TYPE := 0;
-    Salary_Difference Employee.Salary%TYPE := New_Salary - Old_Salary;
+    Salary_Difference Employee.Salary%TYPE;
 BEGIN
     IF INSERTING THEN
         New_Salary := :NEW.Salary;
     ELSIF UPDATING THEN
         Old_Salary := :OLD.Salary;
         New_Salary := :NEW.Salary;
+        Salary_Difference := :NEW.Salary - :OLD.Salary;
+        DBMS_OUTPUT.PUT_LINE('Salary Difference = ' || Salary_Difference);
     ELSIF DELETING THEN
         Old_Salary := :OLD.Salary;
     END IF;
 
     DBMS_OUTPUT.PUT_LINE('Old Salary = ' || Old_Salary);
     DBMS_OUTPUT.PUT_LINE('New Salary = ' || New_Salary);
-    DBMS_OUTPUT.PUT_LINE('Salary Difference = ' || Salary_Difference);
 END;
 
--- Test INSERT operation
+-- Insert a record
 INSERT INTO Employee VALUES (1, 'Adam', 5000);
 
--- Test UPDATE operation
+-- Update a record
 UPDATE Employee SET Salary = 7500 WHERE Id = 1;
 
--- Test DELETE operation
+-- Delete a record
 DELETE FROM Employee WHERE Id = 1;
